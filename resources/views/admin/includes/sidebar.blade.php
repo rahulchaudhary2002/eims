f21cdb39d076556f89ae80fdb34afcbd239377ad
Rahul Chaudhary <rahulchaudharyrbc@gmail.com>
2026-02-12 16:53:27 +0545

    chore: update sidebar design

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -2,97 +2,94 @@
 $sidebarOpen = session('sidebarOpen', true);
 @endphp
 
-<div class="fixed left-0 top-0 h-full bg-white border-r transition-all duration-300 {{ $sidebarOpen ? 'w-[250px]' : 'w-[70px]' }}">
-    <div class="flex items-center justify-center h-[70px] px-4 border-b">
-        <a href="{{ route('admin.dashboard') }}">
-            <!-- @if($sidebarOpen)
-            <img src="{{ asset('images/logo.png') }}" class="h-[50px]" alt="Logo">
-            @else
-            <img src="{{ asset('images/half-logo.png') }}" class="h-[40px]" alt="Logo">
-            @endif -->
-            EIMS
+<div class="fixed left-0 top-0 h-full bg-gradient-to-b from-primary to-secondary border-r transition-all duration-300 {{ $sidebarOpen ? 'w-[250px]' : 'w-[70px]' }}">
+    <div class="flex items-center justify-center text-white text-3xl h-[70px] px-4">
+        <a class="flex items-center font-bold" href="{{ route('admin.dashboard') }}">
+            <x-lucide-graduation-cap class="w-10 h-10 mr-2 text-success" />
+            <span>Sikuna.com</span>
         </a>
     </div>
 
     <ul class="scrollbar w-full h-[calc(100%-70px)] overflow-auto">
         {{-- Dashboard --}}
-        <li class="text-gray-600 font-semibold text-sm">
+        <li class="text-white font-semibold text-sm">
             <a href="{{ route('admin.dashboard') }}"
-                class="flex items-center px-4 py-3 h-12 transition-all duration-300
-                {{ request()->routeIs('admin.dashboard') ? 'text-blue-500' : 'hover:text-blue-500' }}">
+                class="flex items-center px-4 py-3 h-12 transition-all duration-300 border-l-4
+                  {{ request()->routeIs('admin.dashboard') ? 'bg-[rgba(255,255,255,0.1)] border-success' : 'border-transparent hover:bg-[rgba(255,255,255,0.1)] hover:border-success' }}">
                 <x-lucide-layout-dashboard class="w-5 h-5 mr-2" />
                 <span>{{ $sidebarOpen ? 'Dashboard' : '' }}</span>
             </a>
         </li>
 
         {{-- Affiliations --}}
-        <li class="text-gray-600 font-semibold text-sm">
+        <li class="text-white font-semibold text-sm">
             <a href="{{ route('admin.affiliation.index') }}"
-                class="flex items-center px-4 py-3 h-12 transition-all duration-300
-                {{ request()->routeIs('admin.affiliation.*') ? 'text-blue-500' : 'hover:text-blue-500' }}">
+                class="flex items-center px-4 py-3 h-12 transition-all duration-300 border-l-4
+                  {{ request()->routeIs('admin.affiliation.*') ? 'bg-[rgba(255,255,255,0.1)] border-success' : 'border-transparent hover:bg-[rgba(255,255,255,0.1)] hover:border-success' }}">
                 <x-lucide-building class="w-5 h-5 mr-2" />
                 <span>{{ $sidebarOpen ? 'Affiliations' : '' }}</span>
             </a>
         </li>
 
         {{-- Institutions --}}
-        <li class="text-gray-600 font-semibold text-sm">
+        <li class="text-white font-semibold text-sm">
             <a href="{{ route('admin.institution.index') }}"
-                class="flex items-center px-4 py-3 h-12 transition-all duration-300
-                {{ request()->routeIs('admin.institution.*') ? 'text-blue-500' : 'hover:text-blue-500' }}">
+                class="flex items-center px-4 py-3 h-12 transition-all duration-300 border-l-4
+                  {{ request()->routeIs('admin.institution.*') ? 'bg-[rgba(255,255,255,0.1)] border-success' : 'border-transparent hover:bg-[rgba(255,255,255,0.1)] hover:border-success' }}">
                 <x-lucide-school class="w-5 h-5 mr-2" />
                 <span>{{ $sidebarOpen ? 'Institutions' : '' }}</span>
             </a>
         </li>
 
         {{-- Levels --}}
-        <li class="text-gray-600 font-semibold text-sm">
+        <li class="text-white font-semibold text-sm">
             <a href="{{ route('admin.level.index') }}"
-                class="flex items-center px-4 py-3 h-12 transition-all duration-300
-                {{ request()->routeIs('admin.level.*') ? 'text-blue-500' : 'hover:text-blue-500' }}">
+                class="flex items-center px-4 py-3 h-12 transition-all duration-300 border-l-4
+                  {{ request()->routeIs('admin.level.*') ? 'bg-[rgba(255,255,255,0.1)] border-success' : 'border-transparent hover:bg-[rgba(255,255,255,0.1)] hover:border-success' }}">
                 <x-lucide-bar-chart-3 class="w-5 h-5 mr-2" />
                 <span>{{ $sidebarOpen ? 'Levels' : '' }}</span>
             </a>
         </li>
 
         {{-- Courses --}}
-        <li class="text-gray-600 font-semibold text-sm">
+        <li class="text-white font-semibold text-sm">
             <a href="{{ route('admin.course.index') }}"
-                class="flex items-center px-4 py-3 h-12 transition-all duration-300
-                {{ request()->routeIs('admin.course.*') ? 'text-blue-500' : 'hover:text-blue-500' }}">
+                class="flex items-center px-4 py-3 h-12 transition-all duration-300 border-l-4
+                  {{ request()->routeIs('admin.course.*') ? 'bg-[rgba(255,255,255,0.1)] border-success' : 'border-transparent hover:bg-[rgba(255,255,255,0.1)] hover:border-success' }}">
                 <x-lucide-book class="w-5 h-5 mr-2" />
                 <span>{{ $sidebarOpen ? 'Courses' : '' }}</span>
             </a>
         </li>
 
         {{-- Vendors --}}
-        <li class="text-gray-600 font-semibold text-sm">
+        <li class="text-white font-semibold text-sm">
             <a href="{{ route('admin.vendor.index') }}"
-                class="flex items-center px-4 py-3 h-12 transition-all duration-300
-                {{ request()->routeIs('admin.vendor.*') ? 'text-blue-500' : 'hover:text-blue-500' }}">
+                class="flex items-center px-4 py-3 h-12 transition-all duration-300 border-l-4
+                  {{ request()->routeIs('admin.vendor.*') ? 'bg-[rgba(255,255,255,0.1)] border-success' : 'border-transparent hover:bg-[rgba(255,255,255,0.1)] hover:border-success' }}">
                 <x-lucide-truck class="w-5 h-5 mr-2" />
                 <span>{{ $sidebarOpen ? 'Vendors' : '' }}</span>
             </a>
         </li>
 
         {{-- Admission Reward --}}
-        <li class="text-gray-600 font-semibold text-sm">
+        <li class="text-white font-semibold text-sm">
             <a href="{{ route('admin.admission.reward.index') }}"
-                class="flex items-center px-4 py-3 h-12 transition-all duration-300
-                    {{ request()->routeIs('admin.admission.reward.*') ? 'text-blue-500' : 'hover:text-blue-500' }}">
+                class="flex items-center px-4 py-3 h-12 transition-all duration-300 border-l-4
+                  {{ request()->routeIs('admin.admission.reward.*') ? 'bg-[rgba(255,255,255,0.1)] border-success' : 'border-transparent hover:bg-[rgba(255,255,255,0.1)] hover:border-success' }}">
                 <x-lucide-award class="w-5 h-5 mr-2" />
                 <span>{{ $sidebarOpen ? 'Admission Reward' : '' }}</span>
             </a>
         </li>
 
         {{-- Admission Commission --}}
-        <li class="text-gray-600 font-semibold text-sm">
+        <li class="text-white font-semibold text-sm">
             <a href="{{ route('admin.admission.commission.index') }}"
-                class="flex items-center px-4 py-3 h-12 transition-all duration-300
-                {{ request()->routeIs('admin.admission.commission.*') ? 'text-blue-500' : 'hover:text-blue-500' }}">
+                class="flex items-center px-4 py-3 h-12 transition-all duration-300 border-l-4
+                  {{ request()->routeIs('admin.admission.commission.*') ? 'bg-[rgba(255,255,255,0.1)] border-success' : 'border-transparent hover:bg-[rgba(255,255,255,0.1)] hover:border-success' }}">
                 <x-lucide-percent class="w-5 h-5 mr-2" />
                 <span>{{ $sidebarOpen ? 'Admission Commission' : '' }}</span>
             </a>
         </li>
+
     </ul>
 </div>
